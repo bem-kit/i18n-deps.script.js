6be5c29e42febc9d734963b28668db7afc7e1c90
Александр invntrm@ <a-x-@users.noreply.github.com>
2016-05-21 01:57:13 +0400

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,27 @@
 const _ = require('lodash');
 
 // Levels source example: yandex github /lego/islands/blob/dev/index.js
+// example getLevels() from lego:
+({ desktop:
+   [ { path: '/Users/invntrm/lego/islands/common.blocks',
+       check: true },
+     { path: '/Users/invntrm/lego/islands/desktop.blocks',
+       check: true } ],
+  'touch-phone':
+   [ { path: '/Users/invntrm/lego/islands/common.blocks',
+       check: true },
+     { path: '/Users/invntrm/lego/islands/touch.blocks',
+       check: true },
+     { path: '/Users/invntrm/lego/islands/touch-phone.blocks',
+       check: true } ],
+  'touch-pad':
+   [ { path: '/Users/invntrm/lego/islands/common.blocks',
+       check: true },
+     { path: '/Users/invntrm/lego/islands/touch.blocks',
+       check: true },
+     { path: '/Users/invntrm/lego/islands/touch-pad.blocks',
+       check: true } ] });
+
 const getLevels  = require('.').getLevels; // . = project root
 
 module.exports = {
